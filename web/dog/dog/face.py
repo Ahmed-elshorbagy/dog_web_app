e09b4ff9e3684e25d8cb7ba6b1cc3a4573d8f8e9
Ahmed-elshorbagy <zoro.250000@gmail.com>
2017-12-21 22:19:40 +0200

    Add files via upload

--- web/dog/dog/face.py
+++ web/dog/dog/face.py
@@ -6,7 +6,7 @@ FACE_DETECTOR_PATH = r"{base_path}/haarcascades/haarcascade_frontalface_alt.xml"
 	base_path=os.path.abspath(os.path.dirname(__file__)))
 EYE_DETECTOR_PATH = r"{base_path}/haarcascades/haarcascade_eye.xml".format(
 	base_path=os.path.abspath(os.path.dirname(__file__)))
-def dog_ear(img,ad="dog_ear"):
+def dog_ear(img,ad="dog ear"):
     if ad=="dog ear":
         imag2 = cv2.imread('ear2.png')
     else:
@@ -47,30 +47,30 @@ def dog_ear(img,ad="dog_ear"):
         except:
             blank_image[0:y+y2, x:x+img2.shape[1]]=img2[-1*(y-y1):,:]
             
-        img2=blank_image
+    img2=blank_image
 
-        eyes = eye_cascade.detectMultiScale(roi_gray)
+        # eyes = eye_cascade.detectMultiScale(roi_gray)
         # for (ex,ey,ew,eh) in eyes:
         #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
         #     cv2.circle(roi_color,(ex+(ew//2),ey+(eh//2)), eh//4, (0,255,255), 1)
 
 
-        rows,cols,channels = img2.shape
-        roi = img[0:rows, 0:cols ]
-        # Now create a mask of logo and create its inverse mask also
-        img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-        ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
-        mask_inv = cv2.bitwise_not(mask)
+    rows,cols,channels = img2.shape
+    roi = img[0:rows, 0:cols ]
+    # Now create a mask of logo and create its inverse mask also
+    img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+    mask_inv = cv2.bitwise_not(mask)
 
-        img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+    img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
 
-        # Take only region of logo from logo image.
-        img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
+    # Take only region of logo from logo image.
+    img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
 
-        # Put logo in ROI and modify the main image
-        dst = cv2.add(img1_bg,img2)
+    # Put logo in ROI and modify the main image
+    dst = cv2.add(img1_bg,img2)
 
-        img[0:rows, 0:cols ] = dst
+    img[0:rows, 0:cols ] = dst
     return img
 
 # cv2.imshow('img',dog_ear(img))
